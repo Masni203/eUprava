797d09c7f95736becdea7d805e3427c49c05879a
Masni203 <masnikosa.el12.2022@uns.ac.rs>
2026-05-11 02:00:18 +0200

    chore(zdravstvo): Spring Boot 3.2 skeleton + Docker (FAZA 1)
    - Java 21, Spring Boot 3.2.5, Maven build
    - Dockerfile sa multi-stage temurin-21 build-om
    - application.yml + dev/docker profile override-i
    - ZdravstvoApplication entry point

---
+++ zdravstvo/src/main/java/com/euprava/zdravstvo/ZdravstvoApplication.java
@@ -0,0 +1,12 @@
+package com.euprava.zdravstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdravstvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdravstvoApplication.class, args);
+    }
+}
